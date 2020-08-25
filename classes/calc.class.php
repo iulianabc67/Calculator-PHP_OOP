983bdf81b6651689e4dd5e583654d9fe8776818b
iulian <44874060+iulianabc67@users.noreply.github.com>
2020-08-25 23:46:01 +0300

    Add files via upload

---
+++ classes/calc.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class Calc {
+
+    public $operator;
+    public $num1;
+    public $num2;
+
+    public function __construct (string $operator, int $num1, int $num2) {
+
+        $this->operator = $operator;
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+        
+    }
+
+    public function calculator() {
+
+        switch ($this->operator) {
+            case '+':
+                $result = $this->num1 + $this->num2;
+                return $result;
+                break;
+            
+            case '-':
+                $result = $this->num1 - $this->num2;
+                return $result;
+                break;
+            
+            case '/':
+                if($this->num2 !== 0) { 
+                    $result = $this->num1 / $this->num2;
+                    return $result;
+                    break;
+                } else {
+                    echo "<h4>Cannot divede by zero</h4>";
+                    exit;
+                }
+
+            case '*':
+                $result = $this->num1 * $this->num2;
+                return $result;
+                break;
+            
+            case '%':
+                $result = $this->num1 % $this->num2;
+                return $result;
+                break;
+            
+            default:
+                echo "Error found";
+                break;
+        }
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+    declare(strict_types = 1);
+    require __DIR__ . '/vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caculator PHP-OOP</title>
+    <style>
+		body {
+			background-color: #222;
+			color: #eee;
+		}
+
+        footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #b3b3b3db;
+            color: #eee;
+            text-align: center;
+        }
+	</style>
+
+</head>
+<body></body>
+
+    <form action="<?=$_SERVER['PHP_SELF'];?>" method="post">
+        <h1>Calculator (PHP_OOP)</h1>
+        <input type="number" name="num1" placeholder="First number">
+        <select name="oper">
+            <!-- <option value="" selected disabled hidden>Select operation</option> -->
+            <option value="+">+ (add)</option>
+            <option value="-">- (substract)</option>
+            <option value="/">/ (divide)</option>
+            <option value="*">* (multiply)</option>
+            <option value="%">% (modulo)</option>
+        </select>
+        <input type="number" name="num2" placeholder="Second number">
+        <button type="submit" name="submit">Calculate</button>
+    </form>
+
+    <?php
+
+        try {
+            if (isset($_POST["oper"], $_POST["num1"], $_POST["num2"])) {
+        
+                $oper = $_POST["oper"];
+                $num1 = $_POST["num1"];
+                $num2 = $_POST["num2"];
+        
+                $calc = new Calc($oper, (int)$num1, (int)$num2);
+        
+                //try {
+                    echo "<h4>The result is: " . $calc->calculator() . "</h4>";
+                    echo "<h4>" . $calc->num1 . " " . $calc->operator . " " . $calc->num2 . " = " . $calc->calculator() . "</h4>";
+                //} catch (TypeError $e) {
+                 //   echo "Error!: " . $e->getMessage();
+                //}
+            } else {
+                echo "<h4>Please input values!</h4>";
+            }
+        } catch (TypeError $e) {
+            echo "Error!: " . $e->getMessage();
+        }
+    
+    ?>
+
+    <footer>
+        <h3><a href="#">View code on GitHub</a></h3>
+    </footer>
+    
+</body>
+</html>
